0c9c02878cd0e678249cb9969a70b19baf10cf81
Tyrese Samuel <3tyrese@gmail.com>
2020-01-13 18:10:08 -0600

    Test Solution

--- Unit Test/Program.cs
+++ Unit Test/Program.cs
@@ -8,9 +8,19 @@ namespace Unit_Test
 {
     class Program
     {
+        public static void Matrix3Multiply()
+        {
+            Matrix3 m3a = new Matrix3();
+            m3a.SetRotateX(3.98f);
+
+            Matrix3 m3c = new Matrix3();
+            m3c.SetRotateZ(9.62f);
+
+            Matrix3 m3d = m3a * m3c;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Wassup Bro");
+            Matrix3Multiply();
             Console.ReadKey();
         }
     }
